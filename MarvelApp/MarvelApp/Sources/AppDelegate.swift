da950cd5a22c63b631d2bb2107efd83d791c0828
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-11-28 22:35:33 -0300

    Update AppDelegate.swift

--- MarvelApp/MarvelApp/Sources/AppDelegate.swift
+++ MarvelApp/MarvelApp/Sources/AppDelegate.swift
@@ -13,14 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
         let viewController = HomeViewController()
         let navigationController = UINavigationController(rootViewController: viewController)
-        
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
-        
         return true
     }
 
